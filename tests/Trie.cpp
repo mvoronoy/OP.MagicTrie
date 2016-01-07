d85961e3da5e3294ffae5ad3f631af0bdbd923b9
Maksym Voroniy <maksym.voroniy@globallogic.com>
2016-01-07 17:39:21 +0200

    debugiing Trie::insert

--- tests/Trie.cpp
+++ tests/Trie.cpp
@@ -29,33 +29,49 @@ void test_TrieCreation(OP::utest::TestResult &tresult)
     tresult.assert_true(1 == trie->nodes_count());
     tresult.assert_true(nav1 == trie->navigator_end());
 }
+template <class Trie, class Map>
+void compare_containers(OP::utest::TestResult &tresult, Trie& trie, Map& map)
+{
+
+}
 void test_TrieInsert(OP::utest::TestResult &tresult)
 {
     auto tmngr1 = OP::trie::SegmentManager::create_new<TransactedSegmentManager>(test_file_name,
         OP::trie::SegmentOptions()
         .segment_size(0x110000));
     typedef Trie<TransactedSegmentManager, double> trie_t;
-
+    std::map<std::string, double> standard;
+    double v_order = 0.0;
     std::shared_ptr<trie_t> trie = trie_t::create_new(tmngr1);
     const std::string stem1(260, 'a');
     /*stem1_deviation1 - 1'a' for presence, 256'a' for exhausting stem container*/
     auto stem1_deviation1(std::string(257, 'a') + "b");
     //insert that reinforce long stem
     auto b1 = std::begin(stem1);
-    tresult.assert_true(trie->insert(b1, std::end(stem1), 0.0), OP_CODE_DETAILS());
+    tresult.assert_true(trie->insert(b1, std::end(stem1), v_order), OP_CODE_DETAILS());
     tresult.assert_true(2 == trie->nodes_count(), "2 nodes must be create for long stems");
     tresult.assert_true(b1 == std::end(stem1));
     tresult.assert_true(trie->size() == 1);
+    standard[stem1] = v_order++;
+    compare_containers(tresult, *trie, standard);
 
     tresult.assert_false(
-        trie->insert(b1 = std::begin(stem1), std::end(stem1), 0.0), 
+        trie->insert(b1 = std::begin(stem1), std::end(stem1), v_order), 
         OP_CODE_DETAILS("Duplicate insert must not be allowed"));
     tresult.assert_true(b1 == std::end(stem1));
-    tresult.assert_true(trie->size() == 1);
+    tresult.assert_true(trie->size() == 2);
     tresult.assert_true(
         trie->insert(b1 = stem1_deviation1.cbegin(), stem1_deviation1.cend(), 0.0), OP_CODE_DETAILS());
     tresult.assert_true(b1 == std::end(stem1_deviation1));
-
+    standard[stem1_deviation1] = v_order++;
+    compare_containers(tresult, *trie, standard);
+    // test behaviour on range
+    const std::string stem2(256, 'b');
+    tresult.assert_true(trie->insert(b1, std::end(stem1), v_order), OP_CODE_DETAILS());
+    tresult.assert_true(2 == trie->nodes_count(), "2 nodes must exists in the system");
+    tresult.assert_true(b1 == std::end(stem1));
+    tresult.assert_true(trie->size() == 3);
+    compare_containers(tresult, *trie, standard);
     
 }
 void test_TrieInsertGrow(OP::utest::TestResult &tresult)
