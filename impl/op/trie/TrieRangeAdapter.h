962286ed3410c037b7a0344edfe80b780b0ecd00
mvoronoy <mvoronoy@gmail.com>
2020-09-23 14:52:49 +0300

    Improve prefixes_continuation_range unit tests

--- impl/op/trie/TrieRangeAdapter.h
+++ impl/op/trie/TrieRangeAdapter.h
@@ -38,5 +38,22 @@ namespace OP
         template <class TTrie>
         using SiblingRangeAdapter = MixAlgorithmRangeAdapter<TTrie,
             typename Ingredient<TTrie>::Find, typename Ingredient<TTrie>::SiblingNext>;
+
+        /**
+        *  Trie specific operation: create range that contains entries of 'from' that are started 
+        * with prefixes enumerated in param 'prefixes'
+        */
+        template <class RangeOfTrie>
+        std::shared_ptr<RangeOfTrie const> prefixes_continuation_range(const std::shared_ptr<RangeOfTrie const>& from, std::shared_ptr<RangeOfTrie const> prefixes) 
+        {
+            static_assert(RangeOfTrie::is_ordered_c, "Expected ordered range for parameters 'from' and 'prefixes'");
+            return from->if_exists(std::move(prefixes), [](const auto& left, const auto& right){
+                    //'right' key is taken from prefixes array so assuming (no assert needed) it shorter
+                    int cmp = left.compare(0, right.size(), right, 0, right.size());
+                    return cmp;
+                });
+        }
+
+
     } //ns:trie
 }//ns:OP
