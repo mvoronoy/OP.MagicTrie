1258019d1f5b7fa2fd4e855b58f00ddf99c36f3f
mvoronoy <mvoronoy@gmail.com>
2022-06-10 17:55:53 +0300

    Separate FactoryBase to standalone include file

---
+++ impl/op/flur/FactoryBase.h
@@ -0,0 +1,12 @@
+#ifndef _OP_FLUR_FACTORYBASE__H_
+#define _OP_FLUR_FACTORYBASE__H_
+#pragma once
+
+namespace OP::flur
+{
+    /** Base for all classes that composes result Sequence */
+    struct FactoryBase {};
+    
+}//ns:OP
+
+#endif //_OP_FLUR_FACTORYBASE__H_
